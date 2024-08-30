c9f0f4e292ce0a83f6085ee595b2c440634763f7
MOHAMMAD_Aima <mohammad.aima2402@gmail.com>
2024-08-30 06:38:43 +0200

    Update Affluence patients par saison.py

--- pages/Affluence patients par saison.py
+++ pages/Affluence patients par saison.py
@@ -127,7 +127,7 @@ if 'dataframes' in st.session_state and st.session_state['dataframes']:
 
         # Add season text inside the colored rectangles
         annotations.append(dict(
-            x=(start_month )/7  if end_month < start_month else (start_month + end_month )/2 ,
+            x=(start_month )/10  if end_month < start_month else (start_month + end_month-2 )/2 ,
             y=min_count - range_margin,  # Position text inside the rectangle
             text=season,
             showarrow=False,
