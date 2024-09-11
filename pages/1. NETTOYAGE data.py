068c0e2721b3725045fee76290ea8d88e32ea43f
MOHAMMAD_Aima <mohammad.aima2402@gmail.com>
2024-09-11 05:30:50 +0200

    Update 1. NETTOYAGE data.py

--- pages/1. NETTOYAGE data.py
+++ pages/1. NETTOYAGE data.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import plotly.colors as pc
 
 # Streamlit page
-st.title("Affluence des patients (H/F) venus au CTAR IPM sur période saisonnière d'une année.")
+st.title("Nettoyage automatique des données avant analyse.")
 
 # Check if any dataframes have been uploaded in the session state
 if 'dataframes' in st.session_state and st.session_state['dataframes']:
@@ -14,4 +14,24 @@ if 'dataframes' in st.session_state and st.session_state['dataframes']:
 
     # Display main page content
     st.header(f"Contenu du fichier avant nettoyage: {list(ipm.keys())[0]}")
-    st.dataframe(ipm.head(10))
+    st.dataframe(ipm.head(6))
+
+    # Button to clean 'dat_consu'
+    if st.button('Nettoyer la colonne dat_consu'):
+        ipm['dat_consu'] = pd.to_datetime(ipm['dat_consu'], format='%d/%m/%Y', errors='coerce')
+        ipm['vacc_vero_date'] = pd.to_datetime(ipm['vacc_vero_date'], format='%d/%m/%Y', errors='coerce')
+        ipm['vacc_sour_date'] = pd.to_datetime(ipm['vacc_sour_date'], format='%d/%m/%Y', errors='coerce')
+        ipm['dat_consu'] = ipm['dat_consu'].fillna(ipm['vacc_vero_date']).fillna(ipm['vacc_sour_date'])
+        st.success("Colonne 'dat_consu' nettoyée avec succès.")
+
+    # Button to clean 'sexe'
+    if st.button('Nettoyer la colonne sexe'):
+        ipm['sexe'] = ipm.groupby('ref_mordu')['sexe'].transform(lambda x: x.ffill().bfill())
+        st.success("Colonne 'sexe' nettoyée avec succès.")
+
+    # Display cleaned data
+    st.header(f"Contenu du fichier après nettoyage: {list(ipm.keys())[0]}")
+    st.dataframe(ipm.head(6))
+
+else:
+    st.warning("Veuillez d'abord télécharger les fichiers CSV sur la page d'accueil.")
