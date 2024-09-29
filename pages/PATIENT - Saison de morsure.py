002aa0e1d22b6d6ac8802bf57579deae49b3bcf5
MOHAMMAD_Aima <mohammad.aima2402@gmail.com>
2024-09-29 16:32:33 +0200

    Update PATIENT - Saison de morsure.py

--- pages/PATIENT - Saison de morsure.py
+++ pages/PATIENT - Saison de morsure.py
@@ -170,7 +170,7 @@ def plot_saison_morsure_ipm(ipm):
     st.plotly_chart(fig, use_container_width=True)
 
 def plot_saison_peripheral(df):
-    df['date_de_consultation'] = pd.to_datetime(df['date_de_consultation'], format='%d/%m/%Y', errors='coerce')
+    df['date_de_consultation'] = pd.to_datetime(df['date_de_consultation'], format='%Y/%m/%d', errors='coerce')
 
     # Apply the function to create the 'season' column
     df['season'] = df['date_de_consultation'].apply(get_season)
