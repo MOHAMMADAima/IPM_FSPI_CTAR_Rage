961cb74fd199543492c74580272d96c5b914477b
MOHAMMAD_Aima <mohammad.aima2402@gmail.com>
2024-09-30 15:31:00 +0200

    Update PATIENT - Animal mordant et mode de vie.py

--- pages/PATIENT - Animal mordant et mode de vie.py
+++ pages/PATIENT - Animal mordant et mode de vie.py
@@ -79,39 +79,34 @@ if 'dataframes' in st.session_state:
             )
 
             return fig
+        
+        def anim_mord(df):
+                df_clean = df.drop_duplicates(subset=['ref_mordu'])
 
-        # If the selected file is the IPM dataset
-        if selected_file == "CTAR_ipmdata20022024_cleaned.csv":
-            df_clean = df.drop_duplicates(subset=['ref_mordu'])
+                # Selection box for animals
+                selected_animal = st.selectbox("Sélectionnez un animal pour voir le type d'animal", options=df_clean['animal'].dropna().unique())
 
-            # Selection box for animals
-            selected_animal = st.selectbox("Sélectionnez un animal pour voir le type d'animal", options=df_clean['animal'].dropna().unique())
+                # Filter DataFrame for the selected animal
+                filtered_df = df_clean[df_clean['animal'] == selected_animal]
 
-            # Add space between select box and plot
-          #  st.markdown("<br>", unsafe_allow_html=True)
+                # Replace typanim labels with mapped values
+                filtered_df['typanim'] = filtered_df['typanim'].map(label_mapping)
 
-            # Filter DataFrame for the selected animal
-            filtered_df = df_clean[df_clean['animal'] == selected_animal]
+                # Plot for typanim (donut chart) first
+                fig_typanim = create_donut_chart(filtered_df, 'typanim', 'count', f"Répartition des types d'animaux pour : {selected_animal} (IPM)")
+                st.plotly_chart(fig_typanim, use_container_width=True)
 
-            # Replace typanim labels with mapped values
-            filtered_df['typanim'] = filtered_df['typanim'].map(label_mapping)
+                # Allow user to select additional animals
+                additional_animals = df_clean['animal'].value_counts().index.tolist()
+                selected_additional = st.multiselect("Sélectionnez d'autres animaux à afficher", options=additional_animals, default=additional_animals[:4])
 
-            # Plot for typanim (donut chart) first
-            fig_typanim = create_donut_chart(filtered_df, 'typanim', 'count', f"Répartition des types d'animaux pour : {selected_animal} (IPM)")
-            st.plotly_chart(fig_typanim, use_container_width=True)
+                # Filter for selected additional animals
+                if selected_additional:
+                    filtered_additional = df_clean[df_clean['animal'].isin(selected_additional)]
+                    fig_additional_animals = create_pie_chart(filtered_additional, 'animal', 'count', "Répartition des espèces responsables de morsures (Animaux Sélectionnés)")
+                    st.plotly_chart(fig_additional_animals, use_container_width=True)
 
-            # Allow user to select additional animals
-            additional_animals = df_clean['animal'].value_counts().index.tolist()
-            selected_additional = st.multiselect("Sélectionnez d'autres animaux à afficher", options=additional_animals, default=additional_animals[:4])
-
-            # Filter for selected additional animals
-            if selected_additional:
-                filtered_additional = df_clean[df_clean['animal'].isin(selected_additional)]
-                fig_additional_animals = create_pie_chart(filtered_additional, 'animal', 'count', "Répartition des espèces responsables de morsures (Animaux Sélectionnés)")
-                st.plotly_chart(fig_additional_animals, use_container_width=True)
-
-        # If the selected file is the CTAR peripheral dataset
-        elif selected_file == "CTAR_peripheriquedata20022024_cleaned.csv":
+        def anim_mord_perif(df):
             df_clean = df.dropna(subset=['espece'])
 
             df_clean = df_clean[~df_clean['dev_carac'].astype(str).str.contains('nan-nan|nan-|nan-|-nan', regex=True)]
@@ -144,6 +139,48 @@ if 'dataframes' in st.session_state:
                 filtered_additional = df_clean[df_clean['espece'].isin(selected_additional)]
                 fig_additional_animals = create_pie_chart(filtered_additional, 'espece', 'count', "Répartition des espèces responsables de morsures (Animaux Sélectionnés)", is_peripherique=True)
                 st.plotly_chart(fig_additional_animals, use_container_width=True)
+            
+
+
+
+# Main Streamlit logic
+if 'dataframes' in st.session_state:
+    dataframes = st.session_state['dataframes']
+
+    # Select a file to analyze from the uploaded files
+    selected_file = st.selectbox("Sélectionnez un fichier pour l'analyse", options=list(dataframes.keys()))
+
+    # Load the selected dataframe
+    if selected_file:
+        df = dataframes[selected_file]
+
+        # If the selected file is the IPM dataset
+        if selected_file == "CTAR_ipmdata20022024_cleaned.csv":
+            anim_mord(df)
+
+        # If the selected file is the peripheral CTAR dataset
+        elif selected_file == "CTAR_peripheriquedata20022024_cleaned.csv":
+            # Drop rows with NaN in 'id_ctar' column
+            df = df.dropna(subset=['id_ctar'])
+
+            # Get the unique CTARs
+            unique_ctars = df['id_ctar'].unique()
+
+            # Separate the "Tous les CTAR" option from the multiselect
+            all_ctars_selected = st.checkbox("Sélectionnez tous les CTARs")
+
+            if not all_ctars_selected:
+                selected_ctars = st.multiselect(
+                    "Sélectionnez un ou plusieurs CTARs",
+                    options=list(unique_ctars))
+                if not selected_ctars:
+                    st.warning("Veuillez sélectionner au moins un CTAR pour afficher l'analyse.")
+                else:
+                    df= df[df['id_ctar'].isin(selected_ctars)]
+                    anim_mord_perif(df)
+            elif all_ctars_selected:  
+                anim_mord_perif(df)
+           
 
 else:
     st.error("Aucun fichier n'a été téléchargé. Veuillez retourner à la page d'accueil pour télécharger un fichier.")
@@ -151,3 +188,4 @@ else:
 # Sidebar container with fixed width
 with st.sidebar.container():
     st.image("Logo-CORAMAD.jpg", use_column_width=True, width=250, caption="FSPI Rage")
+
