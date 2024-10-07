aea0a3503726c29c58ffef2d6cd21b7576b924d7
MOHAMMAD_Aima <mohammad.aima2402@gmail.com>
2024-10-07 02:50:55 +0200

    Update PATIENT-Morsure Transdermique.py

--- pages/PATIENT-Morsure Transdermique.py
+++ pages/PATIENT-Morsure Transdermique.py
@@ -283,25 +283,42 @@ if 'dataframes' in st.session_state:
 
         # BDD CTAR périphériques
         elif selected_file == "CTAR_peripheriquedata20022024_cleaned.csv":
+             #  Ne pas comptabiliser les lignes sans ID 'id_ctar' = CTAR périphériques inconnues 
             df = df.dropna(subset=['id_ctar'])
+            df = df.dropna(subset=['date_de_consultation'])
 
             # Liste des CTARs périphériques pour leur sélection
             unique_ctars = df['id_ctar'].unique()
 
+            df['date_de_consultation']=pd.to_datetime(df['date_de_consultation'])
+
+            df['Annee'] = df['date_de_consultation'].dt.year
+            df['Annee']=df['Annee'].astype(int)
+            df=df[df['Annee']<=2024]
+            unique_year=df['Annee'].unique()
+
             # Analyse de l'ensemble des CTAR périphériques
             all_ctars_selected = st.checkbox("Sélectionnez tous les CTARs")
+            selected_year = st.multiselect(
+                    "Sélectionnez une ou plusieurs année(s)",
+                    options=list(unique_year))
 
             if not all_ctars_selected:
                 selected_ctars = st.multiselect(
                     "Sélectionnez un ou plusieurs CTARs",
                     options=list(unique_ctars))
-                if not selected_ctars:
-                    st.warning("Veuillez sélectionner au moins un CTAR pour afficher l'analyse.")
+                if not selected_ctars or not selected_year:
+                    st.warning("Veuillez sélectionner au moins un CTAR et une année pour afficher l'analyse.")
                 else:
-                    df= df[df['id_ctar'].isin(selected_ctars)]
+                    df= df[df['id_ctar'].isin(selected_ctars)&df['Annee'].isin(selected_year)]
+                    st.info("Cliquez sur agrandir l'image en haut à droite du graphique.")
                     plot_MT_peripheral(df)
-            elif all_ctars_selected:  
-                plot_MT_peripheral(df)
+            elif all_ctars_selected and selected_year:  
+                st.info("Cliquez sur agrandir l'image en haut à droite du graphique.")
+                df= df[df['Annee'].isin(selected_year)]
+                plot_MT_peripheral(df) 
+            elif all_ctars_selected and not selected_year:
+                st.warning("Veuillez sélectionner au moins une année pour afficher l'analyse.")
            
 
 else:
